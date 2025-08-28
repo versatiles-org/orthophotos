991b836c5723f545c5636243468c8c8c1f11695c
Michael Kreil <github@michael-kreil.de>
2025-08-28 10:36:03 +0000

    finish de/hb

--- de/hb/2_build_vrt.sh
+++ de/hb/2_build_vrt.sh
@@ -1,4 +1,4 @@
 set -e
 
-gdalbuildvrt -addalpha -a_srs "EPSG:25832" hb.vrt tiles_hb/*.jpg
-gdalbuildvrt -addalpha bhv.vrt tiles_bhv/*.tif
+gdalbuildvrt -addalpha -a_srs "EPSG:25832" tiles_hb.vrt tiles_hb/*.jpg
+gdalbuildvrt -addalpha tiles_bhv.vrt tiles_bhv/*.tif
--- de/hb/4_convert.sh
+++ de/hb/4_convert.sh
@@ -1,7 +1,7 @@
 set -e
 
-versatiles convert hb.vpl hb.versatiles
-versatiles convert hb.versatiles hb.mbtiles
+versatiles convert tiles_hb.vpl tiles_hb.versatiles
+versatiles convert tiles_hb.versatiles tiles_hb.mbtiles
 
-versatiles convert bhv.vpl bhv.versatiles
-versatiles convert bhv.versatiles bhv.mbtiles
+versatiles convert tiles_bhv.vpl tiles_bhv.versatiles
+versatiles convert tiles_bhv.versatiles tiles_bhv.mbtiles

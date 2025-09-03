899501633f3eca5a67d5eb579809267d4fccf972
Michael Kreil <git@michael-kreil.de>
2025-09-03 12:25:21 +0000

    update fetch script for de/by

--- regions/de/by/1_fetch.sh
+++ regions/de/by/1_fetch.sh
@@ -1,20 +1,29 @@
 set -e
 
 
-curl -so kreis.kml "https://geodaten.bayern.de/odd/a/dop20/meta/kml/kreis.kml"
-sed -nE 's/.*href=&#34;(.*?\.meta4)&#34;.*/\1/p' kreis.kml > kreise_urls.txt
+if [ ! -f gemeinde_urls.txt ]; then
+  echo "Fetching gemeinde URLs..."
+  curl -so gemeinde.kml "https://geodaten.bayern.de/odd/a/dop20/meta/kml/gemeinde.kml"
+  grep -Eo 'https://geodaten\.bayern\.de/odd/a/dop20/meta/metalink/[0-9]+\.meta4' gemeinde.kml | sort -u > gemeinde_urls.txt
+fi
 
-cat kreise_urls.txt | shuf | parallel --eta --bar -j 4 --lb 'curl -s {}' > meta_files.txt
-sed -nE 's/.*<url>(https:\/\/download2.bayernwolke.de\/a\/dop20\/data\/.*?\.tif)<\/url>.*/\1/p' meta_files.txt | sort -u > dop20_urls.txt
+if [ ! -f tile_urls.txt ]; then
+  echo "Fetching tile URLs..."
+  cat gemeinde_urls.txt | shuf | parallel --eta --bar -j 16 '
+    curl -s {} | grep -Eo 'https://download1.bayernwolke.de/a/dop20/data/[0-9_]+.tif'
+  ' | sort -u > tile_urls.tmp
+  mv tile_urls.tmp tile_urls.txt
+fi
 
 mkdir -p $DATA/tiles
-cat dop20_urls.txt | shuf | parallel --eta --bar -j 16 '
+echo "Downloading tiles..."
+cat tile_urls.txt | shuf | parallel --eta --bar -j 16 '
   set -e
   URL={}
-  ID={/.}
+  ID=$(basename $URL .tif)
   [ -f "$DATA/tiles/$ID.jp2" ] && exit 0
   curl -so $ID.tif $URL
-  gdal_translate $ID.tif $ID.jp2 -co QUALITY=100
+  gdal_translate --quiet $ID.tif $ID.jp2
   mv $ID.jp2 $DATA/tiles/
-  rm $ID.tif
+  rm $ID.*
 '

d13d9e9f40bd6f2f756be6cdf9023f953b4eb8a6
Michael Kreil <git@michael-kreil.de>
2025-09-03 17:20:15 +0000

    add missing WCS service URL to fetch script

--- regions/pl/1_fetch.sh
+++ regions/pl/1_fetch.sh
@@ -1,1 +1,3 @@
 http://mapy.geoportal.gov.pl/wss/service/ATOM/httpauth/download/?fileId=ae73c0632c4bd6407dd03ea9998a180f&name=polska_oi_2021.zip
+
+https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WCS/TrueOrto?Request=GetCapabilities&service=WCS

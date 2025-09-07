abd8af3b65e7ad69487b1b3e92c7026c8ebb3ac6
Michael Kreil <git@michael-kreil.de>
2025-09-07 12:49:30 +0000

    refactor: update build script to dynamically read tile sources from status.yml

--- regions/fr/2_build_vrt.sh
+++ regions/fr/2_build_vrt.sh
@@ -1,14 +1,9 @@
 set -e
 
-for name in \
-  tiles_LAMB93 \
-  tiles_RGAF09UTM20 \
-  tiles_RGFG95UTM22 \
-  tiles_RGM04UTM38S \
-  tiles_RGR92UTM40S \
-  tiles_RGSPM06U21 \
-  tiles_UTM01SW84
-do
+cwd="$(dirname "$0")"
+sources=$(yq -r '.data[]' "$cwd/status.yml")
+
+for name in $sources; do
   echo "Processing $name"
   gdalbuildvrt -addalpha $name.vrt $name/*.jp2
 done

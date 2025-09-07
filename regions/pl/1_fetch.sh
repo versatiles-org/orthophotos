34971a618de9b0f5403a33d3821cf734bc93cef8
Michael Kreil <git@michael-kreil.de>
2025-09-07 12:30:48 +0000

    skip poland :/

--- regions/pl/1_fetch.sh
+++ regions/pl/1_fetch.sh
@@ -1,3 +1,5 @@
-http://mapy.geoportal.gov.pl/wss/service/ATOM/httpauth/download/?fileId=ae73c0632c4bd6407dd03ea9998a180f&name=polska_oi_2021.zip
-
-https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WCS/TrueOrto?Request=GetCapabilities&service=WCS
+# http://mapy.geoportal.gov.pl/wss/service/ATOM/httpauth/download/?fileId=ae73c0632c4bd6407dd03ea9998a180f&name=polska_oi_2021.zip
+# https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WCS/TrueOrto?Request=GetCapabilities&service=WCS
+# https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/TrueOrtho
+# https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution
+# https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMTS/StandardResolution

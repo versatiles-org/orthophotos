1894eb9749db3fd9ed48f67c1b5b5f7edbf7a1bd
Michael Kreil <git@michael-kreil.de>
2025-09-18 20:37:40 +0000

    fix: store de/th as JPEG2000 tiles

--- regions/de/th/1_fetch.sh
+++ regions/de/th/1_fetch.sh
@@ -6,12 +6,23 @@ mkdir -p $DATA/tiles
 # hardcoded sequence for 2024 orthophotos:
 seq 550991 551707 | shuf | parallel --eta --bar -j 4 '
   set -e
-  ID={.}
-  [ -f "$DATA/tiles/{}" ] && exit 0
-  curl -sko "{}.tmp" "https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/download.php?type=op&id={}"
+  ID={}
+  [ -f "$DATA/tiles/$ID.jp2" ] && exit 0
+  
+  curl -sko "$ID.tmp" "https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/download.php?type=op&id=$ID"
   mv "$ID.tmp" "$ID.zip"
+  
   unzip -qo "$ID.zip" -d "$ID"
   rm "$ID.zip"
-  mv $ID/*.tif "$DATA/tiles/"
-  rm -rf $ID/*
+
+  TIF=$(find "./$ID/" -name "*.tif")
+  COUNT=$(echo "$TIF" | wc -l)
+  if [ "$COUNT" -ne 1 ]; then
+    echo "more or less than 1 TIFs in $ID found: $COUNT"
+    exit 1
+  fi
+  gdal_translate --quiet "$TIF" "$ID.jp2"
+  mv "$ID.jp2" "$DATA/tiles/"
+  rm -rf $ID
+  rm -f $ID.*
 '

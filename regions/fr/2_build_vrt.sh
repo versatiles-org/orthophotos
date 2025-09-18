34daf41ddba8e7565baceaea6915a8af8a7159d7
Michael Kreil <git@michael-kreil.de>
2025-09-18 20:24:42 +0000

    fix: fr - better variable name in 2_build_vrt.sh

--- regions/fr/2_build_vrt.sh
+++ regions/fr/2_build_vrt.sh
@@ -3,7 +3,7 @@ set -e
 cwd="$(dirname "$0")"
 sources=$(yq -r '.data[]' "$cwd/status.yml")
 
-for name in $sources; do
-  echo "Processing $name"
-  gdalbuildvrt -addalpha $name.vrt $name/*.jp2
+for source in $sources; do
+  echo "Processing $source"
+  gdalbuildvrt -addalpha $source.vrt $source/*.jp2
 done

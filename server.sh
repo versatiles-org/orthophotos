dabf5413847bb62ed7091e5a80bf691172562985
Michael Kreil <github@michael-kreil.de>
2025-09-18 19:05:31 +0200

    fix: index.html layout

--- server.sh
+++ server.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 set -ex
 
 source ./config.env
-versatiles serve -p 8080 -s web -s ${dir_data}frontend-dev.br.tar.gz [sat]${dir_data}orthophotos.vpl
+versatiles serve -p 8080 -s web -s ${dir_data}frontend-dev.br.tar.gz [orthophotos]${dir_data}orthophotos.vpl

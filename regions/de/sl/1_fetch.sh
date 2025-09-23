e24cb6118cf841d3d29195080f60fa698c0428a4
Michael Kreil <git@michael-kreil.de>
2025-09-23 23:25:12 +0000

    fix (de/sl): switch to new data source to address #6

--- regions/de/sl/1_fetch.sh
+++ regions/de/sl/1_fetch.sh
@@ -2,7 +2,7 @@ set -e
 
 if [ ! -f atom.xml ]; then
   echo "Fetching atom.xml..."
-  curl -so atom.xml "https://geoportal.saarland.de/mapbender/php/mod_inspireDownloadFeed.php?id=b92a9769-caf0-497d-9996-2be0a045ef62&type=DATASET&generateFrom=wmslayer&layerid=49554"
+  curl -so atom.xml "https://geoportal.saarland.de/mapbender/php/mod_inspireDownloadFeed.php?id=6c2eb4e4-6b85-4858-93f8-b62a2ef87987&type=DATASET&generateFrom=wmslayer&layerid=46303"
 fi
 
 xmlstarlet sel -N a="http://www.w3.org/2005/Atom" \
@@ -13,7 +13,7 @@ xmlstarlet sel -N a="http://www.w3.org/2005/Atom" \
 
 echo "Fetching tiles..."
 mkdir -p $DATA/tiles
-cat entries.txt | shuf | parallel --eta --bar -j 1 '
+cat entries.txt | shuf | parallel --eta --bar -j 4 '
   set -e
   URL=$(echo {} | cut -d"|" -f1)
   ID=$(echo {} | cut -d"|" -f2)

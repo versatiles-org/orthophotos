c75d27e19889c6ab3ee30ca343f02003850ad8e6
Michael Kreil <git@michael-kreil.de>
2025-09-23 13:21:20 +0000

    fix (fr): add srcnodata and band options to gdalbuildvrt command

--- regions/fr/2_build_vrt.sh
+++ regions/fr/2_build_vrt.sh
@@ -5,5 +5,5 @@ sources=$(yq -r '.data[]' "$cwd/status.yml")
 
 for source in $sources; do
   echo "Processing $source"
-  gdalbuildvrt -addalpha $source.vrt $source/*.jp2
+  gdalbuildvrt -srcnodata "255 255 255" -b 1 -b 2 -b 3 -addalpha $source.vrt $source/*.jp2
 done

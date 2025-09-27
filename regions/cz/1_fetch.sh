4448c714a0179691a0c82d9da16c97179e66f7ca
Michael Kreil <git@michael-kreil.de>
2025-09-27 10:37:57 +0000

    fix (cz): generate alpha channel, close #6

--- regions/cz/1_fetch.sh
+++ regions/cz/1_fetch.sh
@@ -3,34 +3,33 @@ set -e
 curl -s "https://atom.cuzk.gov.cz/OI/OI.xml" >atom.xml
 xmlstarlet sel -N a="http://www.w3.org/2005/Atom" -t -m '//a:entry//a:id' -v '.' -n atom.xml > urls.txt
 
-mkdir -p $DATA/tiles
-mkdir -p $DATA/alpha
+mkdir -p $DATA/tiles_rgb
+mkdir -p $DATA/tiles_alpha
 
-cat urls.txt | shuf | parallel --eta --bar -j 4 '
+cat urls.txt | shuf | parallel --eta --bar -j 60% '
   set -e
   url_xml={}
   id=$(echo $url_xml | grep -oE "[0-9]+_[0-9]+")
-  if [ ! -f "$DATA/tiles/$id.jp2" ]; then
+  if [ ! -f "$DATA/tiles_rgb/$id.jp2" ]; then
     curl -s "$url_xml" > "$id.xml"
     url_zip=$(xmlstarlet sel -N a="http://www.w3.org/2005/Atom" -t -m '//a:entry//a:id' -v '.' -n $id.xml | head -n 1)
     curl -s "$url_zip" > "$id.zip"
     unzip -qod "$id" "$id.zip"
     rm "$id.zip"
-    mv "$id/$id.j2w" "$DATA/tiles/"
-    mv "$id/$id.jp2" "$DATA/tiles/"
+    mv "$id/$id.j2w" "$DATA/tiles_rgb/"
+    mv "$id/$id.jp2" "$DATA/tiles_rgb/"
     rm -r "$id"
     rm $id.*
   fi
 
-  if [ ! -f "$DATA/alpha/$id.tif" ]; then
+  if [ ! -f "$DATA/tiles_alpha/$id.jp2" ]; then
     gdal raster calc \
-      -i "A=$DATA/tiles/$id.jp2" \
+      -i "A=$DATA/tiles_rgb/$id.jp2" \
       --calc="255*(((A[1]<254)+(A[2]<254)+(A[3]<254))>0)" \
       --overwrite \
       --datatype=Byte \
-      --co TILED=YES --co COMPRESS=DEFLATE \
-      -o "$id.tif"
+      -o "$id.jp2"
       
-    mv "$id.tif" "$DATA/alpha/"
+    mv "$id.jp2" "$DATA/tiles_alpha/"
   fi
 '
--- regions/cz/2_build_vrt.sh
+++ regions/cz/2_build_vrt.sh
@@ -1,3 +1,12 @@
 set -e
 
-gdalbuildvrt -srcnodata "255 255 255" -b 1 -b 2 -b 3 -a_srs "EPSG:3045" tiles.vrt tiles/*.jp2
+gdalbuildvrt -b 1 -b 2 -b 3 -a_srs "EPSG:3045" tiles_rgb.vrt tiles_rgb/*.jp2
+gdalbuildvrt -b 1 -a_srs "EPSG:3045" tiles_alpha.vrt tiles_alpha/*.jp2
+gdalbuildvrt -separate tiles.vrt tiles_rgb.vrt tiles_alpha.vrt
+
+xmlstarlet ed -L \
+  -s "/VRTDataset/VRTRasterBand[@band='1'][not(ColorInterp)]" -t elem -n ColorInterp -v "Red" \
+  -s "/VRTDataset/VRTRasterBand[@band='2'][not(ColorInterp)]" -t elem -n ColorInterp -v "Green" \
+  -s "/VRTDataset/VRTRasterBand[@band='3'][not(ColorInterp)]" -t elem -n ColorInterp -v "Blue" \
+  -s "/VRTDataset/VRTRasterBand[@band='4'][not(ColorInterp)]" -t elem -n ColorInterp -v "Alpha" \
+  tiles.vrt

626d5f515930fe2bcdc370585247a7c34e447b4d
Michael Kreil <git@michael-kreil.de>
2025-09-27 12:57:22 +0000

    fix #2: remove white border from de/sn

--- regions/de/sn/1_fetch.sh
+++ regions/de/sn/1_fetch.sh
@@ -1,11 +1,9 @@
 set -e
 
-if [ ! -f atom.xml ]; then
-  echo "Fetching atom.xml..."
-  curl -so atom.xml "https://geodownload.sachsen.de/inspire/oi_atom/Dataset_oi_tif.xml"
-fi
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-xmlstarlet sel -N a="http://www.w3.org/2005/Atom" -t -m '//a:entry//a:link' -v '@href' -n atom.xml | sed 's/amp;//g' > urls.txt
+# Source: https://www.geodaten.sachsen.de/batch-download-4719.html
+cp "$SCRIPT_DIR/urls.txt" "."
 
 echo "Fetching tiles..."
 mkdir -p $DATA/tiles
@@ -13,17 +11,16 @@ cat urls.txt | shuf | parallel --eta --bar -j 4 '
   set -e
   
   URL={}
-  # quick and dirty fix to repair the URLs in the atom feed
-  URL=$(echo $URL | sed "s/index.php\/s\/UPR5mffOTv5kTiO\/download?path=%2F&files=/public.php\/dav\/files\/QQFLq6nkoSnqB5g\//")
-  
-  ID=$(echo $URL | sed -E "s/.*\/(dop20rgb_.*?+)_tiff\.zip/\1/")
+  ID=$(echo $URL | sed -E "s/.*\/(dop20rgb_.*?)_2_sn_tiff\.zip/\1/")
 
   [ -f "$DATA/tiles/$ID.jp2" ] && exit 0
 
   curl -so "$ID.zip" "$URL"
   unzip -oq "$ID.zip"
+  rm "$ID.zip"
 
-  gdal_translate --quiet -of JP2OpenJPEG "$ID.tif" "$ID.jp2"
+  gdal raster edit --nodata 255 "${ID}_2_sn.tif"
+  gdal_translate --quiet -b 1 -b 2 -b 3 -b mask -colorinterp_4 alpha "${ID}_2_sn.tif" "$ID.jp2"
 
   mv "$ID.jp2" "$DATA/tiles/"
 

79f00999646fd53799c5ae8ad280081030f668f6
Michael Kreil <git@michael-kreil.de>
2025-09-28 12:14:51 +0000

    fix: refactor tile fetching process to generate query URLs dynamically, #16

--- regions/de/th/1_fetch.sh
+++ regions/de/th/1_fetch.sh
@@ -1,10 +1,43 @@
 set -e
 
-echo "Fetching tiles..."
-mkdir -p $DATA/tiles
+if [ ! -f features.jsonl ]; then
+  miny=5561000
+  maxy=5727000
+  minx=557000
+  maxx=769000
+  step=2000
 
-# hardcoded sequence for 2024 orthophotos:
-{ seq 549560 550273; seq 550991 551707; seq 562890 570961; seq 581473 583553; seq 588341 596566; } | shuf | parallel --eta --bar -j 4 '
+  : > query_urls.txt
+  for ((x0=minx; x0<maxx; x0+=step)); do
+    for ((y0=miny; y0<maxy; y0+=step)); do
+      x1=$((x0+step))
+      y1=$((y0+step))
+
+      echo "${x1}_${y1} https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/_ajax/overview.php?crs=EPSG%3A25832&bbox%5B%5D=${x0}&bbox%5B%5D=${y0}&bbox%5B%5D=${x1}&bbox%5B%5D=${y1}&type%5B%5D=op" >> query_urls.txt
+    done
+  done
+
+  mkdir -p query_results
+  echo "  Fetching list of tiles..."
+  cat query_urls.txt | shuf | parallel --eta --bar -j 4 '
+    set -e
+    ID=$(echo {} | cut -d" " -f1)
+    URL=$(echo {} | cut -d" " -f2-)
+    OUT="query_results/$ID.json"
+    [ -f "$OUT" ] && exit 0
+    curl -s $URL > "$OUT.tmp"
+    mv "$OUT.tmp" "$OUT"
+  '
+
+  cat query_urls.txt | shuf | cut -d" " -f1 | parallel --eta --bar -j 8 '
+    set -e
+    jq -rc ".result.features[].properties" "query_results/{}.json"
+  ' | sort -u > features.jsonl
+fi
+
+jq -rs 'group_by(.bildnr) | map(max_by(.bildflugnr|tonumber) | .gid) | .[]' features.jsonl > gids.txt
+
+cat gids.txt | shuf | parallel --eta --bar -j 4 '
   set -e
   ID={}
   [ -f "$DATA/tiles/$ID.jp2" ] && exit 0

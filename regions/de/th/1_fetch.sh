c1014ffb1fb737762ea20eb2ec5d13da43c43c9f
Michael Kreil <git@michael-kreil.de>
2025-09-28 16:40:32 +0000

    fix (de/th): rewrite, because some tiles are black and some are white, #16

--- regions/de/th/1_fetch.sh
+++ regions/de/th/1_fetch.sh
@@ -1,60 +1,54 @@
 set -e
 
-if [ ! -f features.jsonl ]; then
-  miny=5561000
-  maxy=5727000
-  minx=557000
-  maxx=769000
-  step=2000
+mkdir -p $DATA/tiles
 
-  : > query_urls.txt
-  for ((x0=minx; x0<maxx; x0+=step)); do
-    for ((y0=miny; y0<maxy; y0+=step)); do
-      x1=$((x0+step))
-      y1=$((y0+step))
+if [ ! -f coords.txt ]; then
+  MINY=5561
+  MAXY=5727
+  MINX=557
+  MAXX=769
 
-      echo "${x1}_${y1} https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/_ajax/overview.php?crs=EPSG%3A25832&bbox%5B%5D=${x0}&bbox%5B%5D=${y0}&bbox%5B%5D=${x1}&bbox%5B%5D=${y1}&type%5B%5D=op" >> query_urls.txt
+  : > coords.tmp
+  for ((X0=MINX; X0<MAXX; X0+=1)); do
+    for ((Y0=MINY; Y0<MAXY; Y0+=1)); do
+      echo "${X0} ${Y0}" >> coords.tmp
     done
   done
-
-  mkdir -p query_results
-  echo "  Fetching list of tiles..."
-  cat query_urls.txt | shuf | parallel --eta --bar -j 4 '
-    set -e
-    ID=$(echo {} | cut -d" " -f1)
-    URL=$(echo {} | cut -d" " -f2-)
-    OUT="query_results/$ID.json"
-    [ -f "$OUT" ] && exit 0
-    curl -s $URL > "$OUT.tmp"
-    mv "$OUT.tmp" "$OUT"
-  '
-
-  cat query_urls.txt | shuf | cut -d" " -f1 | parallel --eta --bar -j 8 '
-    set -e
-    jq -rc ".result.features[].properties" "query_results/{}.json"
-  ' | sort -u > features.jsonl
+  mv coords.tmp coords.txt
 fi
 
-jq -rs 'group_by(.bildnr) | map(max_by(.bildflugnr|tonumber) | .gid) | .[]' features.jsonl > gids.txt
-
-cat gids.txt | shuf | parallel --eta --bar -j 4 '
+cat coords.txt | shuf | parallel --eta --bar -j 4 '
   set -e
-  ID={}
+  X0=$(echo {} | cut -d" " -f1)
+  Y0=$(echo {} | cut -d" " -f2)
+  ID="32${X0}_${Y0}"
+
   [ -f "$DATA/tiles/$ID.jp2" ] && exit 0
+  [ -f "$DATA/tiles/$ID.skip" ] && exit 0
+
+  URL="https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/_ajax/overview.php?crs=EPSG%3A25832&bbox%5B%5D=$((X0*1000))&bbox%5B%5D=$((Y0*1000))&bbox%5B%5D=$(((X0+1)*1000))&bbox%5B%5D=$(((Y0+1)*1000))&type%5B%5D=op"
+  curl -s $URL > "$ID.json"
+
+  GID=$(jq -rc "[.result.features[].properties | select(.bildnr == \"$ID\")] | max_by(.bildflugnr).gid" "$ID.json")
   
-  curl -sko "$ID.tmp" "https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/download.php?type=op&id=$ID"
-  mv "$ID.tmp" "$ID.zip"
+  [ "$GID" == "null" ] && {
+    touch "$DATA/tiles/$ID.skip";
+    rm -f "$ID.json";
+    exit 0;
+  }
   
+  curl -sko "$ID.zip" "https://geoportal.geoportal-th.de/gaialight-th/_apps/dladownload/download.php?type=op&id=$GID"
+
   unzip -qo "$ID.zip" -d "$ID"
   rm "$ID.zip"
 
   TIF=$(find "./$ID/" -name "*.tif")
-  COUNT=$(echo "$TIF" | wc -l)
-  if [ "$COUNT" -ne 1 ]; then
-    echo "more or less than 1 TIFs in $ID found: $COUNT"
+  if [ $(echo "$TIF" | wc -l) -ne 1 ]; then
+    echo "more or less than 1 TIFs in $ID found"
     exit 1
   fi
-  gdal_translate --quiet "$TIF" "$ID.jp2"
+  gdal_translate --quiet -b 1 -b 2 -b 3 -b mask -colorinterp_4 alpha "$TIF" "$ID.jp2"
+
   mv "$ID.jp2" "$DATA/tiles/"
   rm -rf $ID
   rm -f $ID.*

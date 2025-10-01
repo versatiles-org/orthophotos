1c752ba268a9590e77a9d47ddf0bbb3b328dea7e
Michael Kreil <github@michael-kreil.de>
2025-10-01 07:30:34 +0000

    feat: add nl

---
+++ regions/nl/1_fetch.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ ! -f features.geojson ]; then
+  echo -e "Fetching features.geojson..."
+  curl -so features.geojson "https://fsn1.your-objectstorage.com/hwh-portal/20230609_tmp/links/nationaal/Nederland/BM_LRL2024O_RGB.json"
+fi
+
+echo "Downloading tiles..."
+mkdir -p "$DATA/tiles"
+jq -r '.features[] | .properties.file' features.geojson | parallel --eta --bar -j 4 '
+  set -e
+  URL={}
+  ID=$(basename "$URL" .tif)
+  curl -so "$ID.tif" "$URL"
+  gdal_translate -q "$ID.tif" "$ID.jp2"
+  mv "$ID.jp2" "$DATA/tiles/"
+  rm -f $ID*
+'

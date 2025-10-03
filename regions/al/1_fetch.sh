1483dbb896e83335604c7468b7d3b5aaec04e7ce
Michael Kreil <github@michael-kreil.de>
2025-10-03 12:58:45 +0000

    fix: set 'white' to transparent for AL

--- regions/al/1_fetch.sh
+++ regions/al/1_fetch.sh
@@ -2,4 +2,4 @@ set -e
 
 source "$(dirname "$0")/../../wms/wms_scrape.sh"
 
-wms_scrape "https://geoportal.asig.gov.al/service/wms" 17 "ortofoto_2018:Ortofoto_2018"
+wms_scrape "https://geoportal.asig.gov.al/service/wms" 17 "ortofoto_2018:Ortofoto_2018" white

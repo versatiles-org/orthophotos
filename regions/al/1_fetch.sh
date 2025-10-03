ad392098ef19cf106cd49fae71aecba38d68afa0
Michael Kreil <github@michael-kreil.de>
2025-10-03 14:47:42 +0000

    fix: update WMS fetch script and status file for AL region

--- regions/al/1_fetch.sh
+++ regions/al/1_fetch.sh
@@ -2,4 +2,5 @@ set -e
 
 source "$(dirname "$0")/../../wms/wms_scrape.sh"
 
-wms_scrape "https://geoportal.asig.gov.al/service/wms" 17 "ortofoto_2018:Ortofoto_2018" white
+wms_scrape "https://geoportal.asig.gov.al/service/wms" 17 "orthophoto_2015:OrthoImagery_20cm" white
+# "ortofoto_2018:Ortofoto_2018" white

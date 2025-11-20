8f08bc785af37f958642197eeb39b47350d15712
yetzt <git@yetzt.me>
2025-11-20 14:21:13 +0100

    add hint for ftp method requiring credentials, clean up

--- regions/dk/1_fetch.sh
+++ regions/dk/1_fetch.sh
@@ -4,5 +4,11 @@ source "$(dirname "$0")/../../wms/wms_scrape.sh"
 
 token=$( curl -s https://dataforsyningen.dk/assets/index-CIBmMQ4i.js | grep -E ',cg="([^"]+)",' | sed 's/^.*cg="//; s/".*$//' )
 
-# wms_scrape "https://api.dataforsyningen.dk/orto_foraar_DAF?crs=EPSG:4326&dpiMode=7&featureCount=10&format=image/png&layers=orto_foraar_12_5&tilePixelRatio=0&url=https://api.dataforsyningen.dk/orto_foraar_DAF&token=$token" 17 Raster black
 wms_scrape "https://api.dataforsyningen.dk/orto_foraar_DAF?token=${token}" 17 orto_foraar_12_5 black
+
+# alternative:
+# user=""; # username for dataforsyningen.dk
+# pass=""; # password
+# curl -s --list-only "ftps://${user}:${passw}@ftp.dataforsyningen.dk/grundlaeggende_landkortdata/ortofoto/12_5CM/" | while read f; do
+# 	curl -s -O "ftps://${user}:${passw}@ftp.dataforsyningen.dk/grundlaeggende_landkortdata/ortofoto/12_5CM/${f}";
+# done;

84e787adcaf9b7a2e96b29601ba29d091d144021
Michael Kreil <github@michael-kreil.de>
2026-01-30 18:29:42 +0100

    fix: correct typo in variable name from 'constainers' to 'containers' in generateVPL function

--- src/server/vpl.ts
+++ src/server/vpl.ts
@@ -6,22 +6,22 @@ export function generateVPL(filename: string) {
 	const srcOrthophotos = resolve(path, 'orthophotos/');
 	const srcSatellite = resolve(path, 'satellite/');
 
-	const constainers: string[] = [];
+	const containers: string[] = [];
 	for (
 		const container of walkSync(srcOrthophotos, { exts: ['.versatiles'], includeDirs: false })
 	) {
-		constainers.push(`from_container filename="${container.path}" | raster_overscale`);
+		containers.push(`from_container filename="${container.path}" | raster_overscale`);
 	}
 
-	constainers.push(
+	containers.push(
 		`from_container filename="${srcSatellite}/s2gm/s2gm_overview.versatiles" | raster_overscale`,
 	);
-	constainers.push(
+	containers.push(
 		`from_container filename="${srcSatellite}/bluemarble/bluemarble.versatiles" | raster_levels gamma=0.8 brightness=0.2 contrast=0.8 | raster_overscale`,
 	);
 
 	const vpl = `from_stacked_raster [
-  		${constainers.join(',\n  ')}
+  		${containers.join(',\n  ')}
 	]
       | filter level_max=19
       | meta_update attribution="Various sources"

46c920ae7446b93470fe7b5588566e2c11e2962f
Michael Kreil <github@michael-kreil.de>
2026-03-17 22:23:05 +0000

    feat: increase concurrency level for tile processing to improve performance

--- src/fetch/regions/de_schleswig_holstein.ts
+++ src/fetch/regions/de_schleswig_holstein.ts
@@ -10,7 +10,7 @@ import { withRetry } from '../../lib/retry.ts';
 
 const ATOM_URL = 'https://service.gdi-sh.de/SH_OpenGBD/feeds/DOP20/DOP20.xml';
 const TILE_XML_BASE = 'https://service.gdi-sh.de/SH_OpenGBD/feeds/DOP20/DOP20_';
-const CONCURRENCY = 4;
+const CONCURRENCY = 16;
 
 const xmlParser = new XMLParser({ ignoreAttributes: false, attributeNamePrefix: '@_' });
 

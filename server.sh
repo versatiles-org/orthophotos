6a1804cfcd7d5bc4b09658cf87de8cef7a465093
Michael Kreil <github@michael-kreil.de>
2026-03-24 18:21:34 +0100

    fix: update shebang and add ssh-identity option for versatiles serve command

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 cd "$(dirname "$0")"
 set -ex
 
 source ./config.env
-versatiles serve -p 8080 -s web -s ${dir_data}frontend-dev.br.tar.gz [orthophotos]${dir_data}orthophotos.vpl
+versatiles serve -p 8080 --ssh-identity "${ssh_id}" -s web -s "${dir_data}frontend-dev.br.tar.gz" "[orthophotos]${dir_data}orthophotos.vpl"
